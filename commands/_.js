2992710eac02448fedf524cec5bb0aaccb245d2d
Bots.Business <git.sync@bots.business>
2021-11-05 06:55:26 +0000

    Bots.Business export for bot id: 480772

---
+++ commands/_.js
@@ -0,0 +1,22 @@
+/*CMD
+  command: *
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+var key = [
+  [
+    { text: "Select Chat", switch_inline_query: "BTC" }
+  ]
+]
+Api.sendMessage({
+  text:
+    "_Hi👋\nYou can use me to get Crypto/Currencies prices, you can convert a Currency to INR and USD Prices, Press the button below and select a chat where to use me.._\n*Live Price:* `@"+bot.name+" BTC INR`\n*Conversion:* `@"+bot.name+" convert.1.BTC.INR`",
+  reply_markup: { inline_keyboard: key }, parse_mode: "Markdown"
+})
+
